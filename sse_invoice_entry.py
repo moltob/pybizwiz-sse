3011448930eba455e340e35ffd63bcfced4079b8
Mike Pagel <mike@mpagel.de>
2017-03-16 07:35:04 +0100

    refactored

--- sse_invoice_entry.py
+++ sse_invoice_entry.py
@@ -7,10 +7,29 @@ entering the corresponding data into the currently active table.
 import datetime
 import json
 import locale
+import time
 import win32gui
 
-import time
 import win32con
+
+SLEEP = 0.04
+
+
+def send_json_export(filepath):
+    locale.setlocale(locale.LC_ALL, '')
+
+    with open(filepath) as file:
+        export = json.load(file)
+    assert export['version'] == 1, 'Unexpected export version.'
+
+    hwnd = find_tax_application_window()
+    if not hwnd:
+        print('Tax application window not found, aborting.')
+        return
+
+    win32gui.SetForegroundWindow(hwnd)
+    for invoice in export['invoices']:
+        send_invoice(hwnd, invoice)
 
 
 def find_tax_application_window():
@@ -29,46 +48,36 @@ def find_tax_application_window():
     return None
 
 
-def export_to_sse(hwnd, invoice):
+def send_invoice(hwnd, invoice):
     number = 'R{}'.format(invoice['number'])
     total = locale.currency(float(invoice['total']), symbol=False)
     name = '{} {}'.format(invoice['first_name'], invoice['last_name']).strip()
     date = datetime.datetime.strptime(invoice['date_paid'], '%Y-%m-%d').strftime('%d.%m.%y')
     print('Entering data for {} {}, {}, {} €.'.format(number, date, name, total))
 
-    send(hwnd, number)
-    send(hwnd, date)
-    send(hwnd, name)
-    send(hwnd, total)
+    send_field(hwnd, number)
+    send_field(hwnd, date)
+    send_field(hwnd, name)
+    send_field(hwnd, total)
 
 
-def send(hwnd, string):
-    sleep = 0.04
+def send_field(hwnd, string):
     for char in string:
-        win32gui.SendMessage(hwnd, win32con.WM_CHAR, ord(char), 0)
-        time.sleep(sleep)
+        send_char(hwnd, char)
 
     # enter next field:
+    send_tab(hwnd)
+
+
+def send_char(hwnd, char):
+    win32gui.SendMessage(hwnd, win32con.WM_CHAR, ord(char), 0)
+    time.sleep(SLEEP)
+
+
+def send_tab(hwnd):
     win32gui.SendMessage(hwnd, win32con.WM_KEYDOWN, win32con.VK_TAB, 0)
-    time.sleep(sleep)
-
-
-def main(filepath):
-    locale.setlocale(locale.LC_ALL, '')
-
-    with open(filepath) as file:
-        export = json.load(file)
-    assert export['version'] == 1, 'Unexpected export version.'
-
-    hwnd = find_tax_application_window()
-    if not hwnd:
-        print('Tax application window not found, aborting.')
-        return
-
-    win32gui.SetForegroundWindow(hwnd)
-    for invoice in export['invoices']:
-        export_to_sse(hwnd, invoice)
+    time.sleep(SLEEP)
 
 
 if __name__ == '__main__':
-    main(r'D:\Home\Downloads\invoices (2).json')
+    send_json_export(r'D:\Home\Downloads\invoices (2).json')
